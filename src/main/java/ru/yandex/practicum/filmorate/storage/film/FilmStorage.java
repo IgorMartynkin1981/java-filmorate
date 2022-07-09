e4c52046c46c004bdfcb7cbf045da21c6adb3a87
IgorMartynkin <imartynkin@yandex.ru>
2022-07-09 20:09:52 +0900

    Refactor - доработал классы FilmStorage & InMemoryFilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,4 +1,15 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+
 public interface FilmStorage {
+    Collection<Film> findAll();
+    Film findFilm(Integer id);
+    Film createFilm(Film film);
+    Film updateFilm(Film film);
+    void deleteFilms();
+    void deleteFilm(Film film);
 }
